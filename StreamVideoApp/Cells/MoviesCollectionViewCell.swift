a7db8610843b6f398ff6971364338b996b8bf50c
iMad <damianos.imad@gmail.com>
2024-02-10 13:14:56 +0200

    implementing table and collection view

--- StreamVideoApp/Cells/MoviesCollectionViewCell.swift
+++ StreamVideoApp/Cells/MoviesCollectionViewCell.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class MoviesCollectionViewCell: UICollectionViewCell {
-
+    class var identifier: String { return String(describing: self) }
+    class var nib: UINib { return UINib(nibName: identifier, bundle: nil) }
+    
+    @IBOutlet weak var coverImage: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- StreamVideoApp/HomeViewController.swift
+++ StreamVideoApp/HomeViewController.swift
@@ -7,23 +7,89 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomePageViewController: UIViewController {
+
+    @IBOutlet weak var horizontalTableView: UITableView!
+    @IBOutlet weak var verticalCollectionView: UICollectionView!
+
+    var horizontalImages: [UIImage] = []
+    var verticalImages: [UIImage] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupTableView()
+        setupCollectionView()
+        loadSampleImages()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupTableView() {
+        horizontalTableView.delegate = self
+        horizontalTableView.dataSource = self
+        horizontalTableView.register(MoviesTableViewCell.nib, forCellReuseIdentifier: MoviesTableViewCell.identifier)
     }
-    */
+
+    func setupCollectionView() {
+        verticalCollectionView.delegate = self
+        verticalCollectionView.dataSource = self
+        verticalCollectionView.register(MoviesCollectionViewCell.nib, forCellWithReuseIdentifier: MoviesCollectionViewCell.identifier)
+//        verticalCollectionView.register(MoviesCollectionViewCell.nib, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: MoviesCollectionViewCell.identifier)
+        let verticalLayout = UICollectionViewFlowLayout()
+        verticalLayout.scrollDirection = .vertical
+        verticalCollectionView.collectionViewLayout = verticalLayout
+    }
+
+    func loadSampleImages() {
+        horizontalImages = [UIImage(named: "sample_image_1")!, UIImage(named: "sample_image_2")!, UIImage(named: "sample_image_3")!]
+        verticalImages = [UIImage(named: "sample_image_4")!, UIImage(named: "sample_image_3")!, UIImage(named: "sample_image_2")!]
+
+        horizontalTableView.reloadData()
+        verticalCollectionView.reloadData()
+    }
+}
+
+extension HomePageViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return verticalImages.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MoviesCollectionViewCell.identifier, for: indexPath)
+
+        // Configure collection view cell
+        cell.backgroundColor = UIColor.lightGray
+
+        let imageView = UIImageView(image: verticalImages[indexPath.item])
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false // Add this line
+
+        cell.contentView.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: cell.contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor)
+        ])
+
+        return cell
+    }
+}
+
+extension HomePageViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return horizontalImages.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MoviesTableViewCell.identifier, for: indexPath) as? MoviesTableViewCell else {
+            // If the cell cannot be dequeued, create a new instance of MoviesTableViewCell
+            let cell = MoviesTableViewCell(style: .default, reuseIdentifier: MoviesTableViewCell.identifier)
+            cell.coverImage.image = horizontalImages[indexPath.item]
+            return cell
+        }
+        // Configure the cell with the corresponding image
+        cell.coverImage.image = horizontalImages[indexPath.item]
+        return cell
+    }
 
 }
