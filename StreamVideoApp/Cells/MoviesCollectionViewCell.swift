55d7d3d2ba73afb31f2eb356c5065dc892268fcf
iMad <damianos.imad@gmail.com>
2024-02-10 13:13:13 +0200

    implenting collection and table view

---
+++ StreamVideoApp/Cells/MoviesCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesCollectionViewCell.swift
+//  StreamVideoApp
+//
+//  Created by iMad on 10/02/2024.
+//
+
+import UIKit
+
+class MoviesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
